a5cbd724048bfd7eacbb68daedcc220fdc612687
S0lll0s <s0lll0s@blinkenshell.org>
2015-10-17 21:10:38 +0200

    Clean up

--- test/main.lua
+++
@@ -1,4 +0,0 @@
-function love.draw()
-    love.graphics.setColor( require("test") )
-    love.graphics.circle( "fill", 50, 50, 30 )
-end
--- test/test.lua
+++
@@ -1,2 +0,0 @@
-print"test loaded"
-return { 0, 255, 255, 255 }
